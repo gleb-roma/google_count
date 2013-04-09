17769816fdab7ed79a385a7dbf6dbd56d44b79c3
Gleb Romanyuk <gleb@gleb-Jing-Vaio.(none)>
2013-04-09 11:32:07 -0400

    provisions, argument added to dumptable.sh

--- brun.sh
+++ brun.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
-bt=pe    # Bing (from Imil)
 
-if [ ! -e b.0 ]; then
-	echo "Starting from b.0"
-	./gblogs.pl -l$bt -zc <lists/$bt-top-exclude-keywords
-	if [ $? -ne 0 ]; then echo "Error in the script"; exit; fi
-	echo 'Press Ctrl+C within 5 secs to stop the bash script'
-	sleep 5
-	touch b.0; chown gleb b.0
-fi
-if [ ! -e b.1 ]; then
-	echo "Starting from b.1"
-	cat lists/$bt-top-include-keywords | ./gblogs.pl -l$bt -z
-	if [ $? -ne 0 ]; then echo "Error in the script"; exit; fi
-	echo 'Press Ctrl+C within 5 secs to stop the bash script'
-	sleep 5
-	touch b.1; chown gleb b.1
-fi
-if [ ! -e b.2 ]; then
-	echo "Starting from b.2"
-	./gblogs.pl -l$bt -z <lists/$bt-top-general
-	if [ $? -ne 0 ]; then echo "Error in the script"; exit; fi
-	echo 'Press Ctrl+C within 5 secs to stop the bash script'
-	sleep 5
-	touch b.2; chown gleb b.2
-fi
-echo "THE END"
+for bt in sk co   # Bing coding (from Imil)
+do
 
+	if [ ! -e b.0 ]; then
+		echo "Starting from b.0"
+		./gblogs.pl -l$bt -zc <lists/$bt-top-exclude-keywords
+		if [ $? -ne 0 ]; then echo "Error in the script"; exit; fi
+		echo 'Press Ctrl+C within 5 secs to stop the bash script'
+		sleep 5
+		touch b.0; chown gleb b.0
+	fi
+	if [ ! -e b.1 ]; then
+		echo "Starting from b.1"
+		cat lists/$bt-top-include-keywords | ./gblogs.pl -l$bt -z
+		if [ $? -ne 0 ]; then echo "Error in the script"; exit; fi
+		echo 'Press Ctrl+C within 5 secs to stop the bash script'
+		sleep 5
+		touch b.1; chown gleb b.1
+	fi
+	if [ ! -e b.2 ]; then
+		echo "Starting from b.2"
+		./gblogs.pl -l$bt -z <lists/$bt-top-general
+		if [ $? -ne 0 ]; then echo "Error in the script"; exit; fi
+		echo 'Press Ctrl+C within 5 secs to stop the bash script'
+		sleep 5
+		touch b.2; chown gleb b.2
+	fi
+	echo "THE END"
+	rm b.*
+done
